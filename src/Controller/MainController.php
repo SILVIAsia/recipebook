1e2a957d4f2c1ac251c9fc929316eebbdd8b7185
SILVIAsia <silvia_yanc@hotmail.com>
2026-05-20 23:40:07 +0200

    first twig

---
+++ src/Controller/MainController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+
+{
+    #[Route("/home", name: "main_home", methods: ["GET"])]
+    public function home(): Response
+    {
+        return $this->render("main/home.html.twig");
+    }
+
+
+    #[Route("/test", name: "main_test", methods: ["GET"])]
+    public function test(): Response
+    {
+        return $this->render("main/test.html.twig");
+    }
+
+
+
+}
